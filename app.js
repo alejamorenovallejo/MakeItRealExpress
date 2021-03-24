682599c91b59369b0ff7faeb5fb7b0899d1d81df
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-03-23 21:05:16 -0500

    Pares e impares

--- app.js
+++ app.js
@@ -7,11 +7,25 @@ const app = express();
   
 });*/
 
-app.get('/makers/:name', (req, res) => {
+/*app.get('/makers/:name', (req, res) => {
   const value = req.params.name || 'desconocido';   
   
   res.send('<h1>Hola ' +  value.charAt(0).toUpperCase() + value.slice(1)  +'!</h1>');
   
+});*/
+
+app.get('/', (req, res) => {
+
+  let value ='';
+  for(i=1;i<=50;i++) {
+     if (i  %  2 === 0) {
+        value += '<p>' + i + ' Soy Par!</p>'
+      }else 
+      {
+        value +='<p>' + i + ' Soy Impar!</p>'
+      }
+  }
+  res.send(value);
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));

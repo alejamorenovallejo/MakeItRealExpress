0d27bff71e3277d096b08fdecc66cc3bea50dcf9
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-03-23 19:18:39 -0500

    Saludame 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const value =req.query.nombre ==='' ? 'desconocido' : req.query.nombre; 
+  const value = req.query.nombre || 'desconocido';   
   res.send('<h1>Hola ' +  value  +'!</h1>');
   
 });

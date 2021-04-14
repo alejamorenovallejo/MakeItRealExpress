40d4e8a339cb50d1bf22c0d73c6d7d1dd2d8e03e
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-04-13 20:31:45 -0500

    Mongo DB

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
 

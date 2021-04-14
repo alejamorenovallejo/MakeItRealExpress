b71fa75e915be8f0947c9609bdf8681077b9725d
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-04-13 20:29:11 -0500

    Mongo DB

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
 
+mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+
+const schema = mongoose.Schema({
+  date : {type: Date, default: Date.now},
+  name : String,
+});
+
+// definimos el modelo
+const Visitor = mongoose.model("Visitor", schema);
 
 app.get('/', (req, res) => {
-  //res.send('<h1>' +  req +'</h1>' )
+  const value = req.query.name || 'Anónimo';  
+  Visitor.create({ name: value }, function(err) {
+    if (err) return console.error(err);
+  });
+  res.send('<h1>El visitante fue almacenado con éxito</h1>');
+  
+});
+
+
+
+/*app.get('/', (req, res) => {
   res.send(req.header('user-agent'))
 
   
-});
+});*/
+
+
 
 
 app.listen(3000, () => console.log('Listening on port 3000!'));

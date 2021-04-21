643cb815e57f54fe85bc8fed9bc147678a77d32f
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-04-21 00:56:57 -0500

    Visitantes Recurrentes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/test', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
 app.set('view engine', 'pug');
@@ -10,34 +10,32 @@ app.set('views', 'views');
 
 const schema = mongoose.Schema({
   name : String,
-  count : Number,
+  count : { type: Number, default: 1 },
 });
 
 // definimos el modelo
 const Visitor = mongoose.model("Visitor", schema);
 
 /*Visitantes recurrentes*/
-app.get('/', (req, res) => {
-  const value = req.query.name
-
-  if (!value) {
-      Visitor.create({ name: (value ? value: 'Anonimo'),  count: 1}, function(err) {
-        if (err) return console.error(err);
+  app.get('/', async (req, res) => {
+    try {
+      const visitorExists = await Visitor.findOne({ name: req.query.name });
+      if (visitorExists && visitorExists.name !== 'Anonimo') {
+        await Visitor.findById(visitorExists.id, (err, visitor) => {
+          visitor.count += 1;
+          visitor.save();
+        });
+      } else {
+        await Visitor.create({ name: req.query.name || 'Anonimo' });
+      }
+      const visitors = await Visitor.find();
+      res.render('index', {
+        visitors : visitors,
       });
-  }else {
-      Visitor.find({ name: value }, function(err, visitors) {
-          if (err) return console.error(err);
-          count = visitors[0].count+1
-          Visitor.updateOne({ count: count }, function(err) {
-              if (err) return console.error(err);
-          });
-      });
-  }
-
-  Visitor.find({}, function(err, visitors) {
-    res.render("index", {
-      visitors: visitors
-    });
-  });
+      console.log(visitorExists)
+    } catch (error) {
+      console.error(error);
+      res.status(500);
+    }
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));

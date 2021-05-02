4a74797a2e40a5e0b5c9ab13dd19b0de3385c78a
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-05-02 15:00:18 -0500

    Formulario Registro

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/register',(req, res) => {
 app.post('/register', async (req,res) => {
   try {
     const hash = await bcrypt.hash(req.body.password, 10);
-    await User.create({name:req.body.nombre, email:req.body.email, password:hash});
+    await User.create({name:req.body.name, email:req.body.email, password:hash});
     res.redirect('/');
     
   } catch (error) {

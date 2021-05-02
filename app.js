1a469f95120cb83a83bc1bc06dfdca0fa2ce330d
Alejandra Moreno Vallejo <alejamorenovallejo@gmail.com>
2021-05-02 16:23:54 -0500

    Autenticame

--- app.js
+++ app.js
@@ -28,6 +28,63 @@ const userSchema = mongoose.Schema({
 // definimos el modelo
 const User = mongoose.model("User", userSchema);
 
+/**Autenticacion**/
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login')
+});
+
+app.post('/login', async (req,res)=>{
+
+  let validation=false;
+
+  try{
+      const usersExist= await User.findOne({'email':req.body.email});
+      if(usersExist){
+          validation=await bcrypt.compare(req.body.password, usersExist.password);
+      }
+
+      if(validation){
+          req.session.user= usersExist.id;
+          const usersExists= await User.find();
+          res.render('users', {"userList":usersExists});
+      }else{
+          res.render('login', {"error":true});
+      }
+
+  }catch(err){
+      console.log(err);
+  }
+  
+});
+
+app.get('/logout', (req,res)=>{
+  res.clearCookie('user');
+  req.session = null;
+  res.redirect('/login');
+});
+
+
+app.get('/register',(req, res) => {
+  res.render('register')
+});
+
+app.post('/register', async (req,res) => {
+try {
+  const hash = await bcrypt.hash(req.body.password, 10);
+  await User.create({name:req.body.name, email:req.body.email, password:hash});
+  res.redirect('/');
+  
+} catch (error) {
+  console.error(error);
+  res.status(500);
+}
+});
+
+/** Formulario Registro
 app.get('/', async (req, res) => {
   try {
     const users = await User.find();
@@ -54,6 +111,6 @@ app.post('/register', async (req,res) => {
     console.error(error);
     res.status(500);
   }
-});
+});**/
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
